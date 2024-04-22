f2f239284bf394c67b10facf083c50639fae632f
SpikeHD <spikegdofficial@gmail.com>
2024-04-22 11:27:34 -0700

    fix: index.html if it exists

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ pub fn main() {
   // Serve anything in the current directory
   app.at("/").serve_dir(&path).expect("Could not serve directory");
 
+  // Also serve the index.html at root if it exists
+  let index = path.join("index.html");
+  if index.exists() {
+    app.at("/").serve_file(index).expect("Could not serve index.html");
+  }
+
   println!("Serving {:?} on http://127.0.0.1:8080", path);
 
   block_on(app.listen("127.0.0.1:8080")).expect("Could not start server");
